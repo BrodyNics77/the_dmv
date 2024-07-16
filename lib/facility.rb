7211237b164bb5ae0a781a9c61029bf8cbbb0f13
BrodyNics77 <brodynichols777@gmail.com>
2024-07-16 09:48:23 -0500

    finishing touches for 2 :)

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,7 +1,7 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
+  def initialize(name, address, phone, services)
     @name = name
     @address = address
     @phone = phone
@@ -10,5 +10,30 @@ class Facility
 
   def add_services(service)
     @services << service
+  end
+
+  def register_vehicles(vehicle)
+    case vehicle.type
+    when :antique
+      vehicle.register(:antique, 25)
+    when :ev
+      vehicle.register(:ev, 200)
+    else
+      vehicle.register(:regular, 100)
+    end
+  end
+
+  def administer_written_test(registrant)
+    if registrant.has_permit? && registrant.age >= 16
+      registrant.pass_written_test
+      registrant.pass_road_test
+      registrant.issue_license
+    end
+  end
+
+  def renew_license(registrant)
+    if registrant.has_license?
+      registrant.renew_license
+    end
   end
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
   end
+
   describe '#initialize' do
     it 'can initialize' do
       expect(@facility).to be_an_instance_of(Facility)
@@ -14,13 +15,49 @@ RSpec.describe Facility do
     end
   end
 
-  describe '#add service' do
+  describe '#add_service' do
     it 'can add available services' do
       expect(@facility.services).to eq([])
       @facility.add_service('New Drivers License')
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe '#register_vehicle' do
+    it 'registers vehicles based on type' do
+      vehicle1 = double("Vehicle", type: :antique)
+      vehicle2 = double("Vehicle", type: :ev)
+      vehicle3 = double("Vehicle", type: :regular)
+
+      expect(vehicle1).to receive(:register).with(:antique, 25)
+      expect(vehicle2).to receive(:register).with(:ev, 200)
+      expect(vehicle3).to receive(:register).with(:regular, 100)
+
+      @facility.register_vehicle(vehicle1)
+      @facility.register_vehicle(vehicle2)
+      @facility.register_vehicle(vehicle3)
+    end
+  end
+
+  describe 'can administer_written_test' do
+    it 'administers written test to eligible registrants' do
+      registrant = double("Registrant", has_permit?: true, age: 16)
+
+      expect(registrant).to receive(:pass_written_test)
+
+      @facility.administer_written_test(registrant)
+    end
+  end
+
+  describe 'can administer_road_test' do
+    it 'can administer road test to eligible registrants' do
+      registrant = double("Registrant", passed_written_test?: true)
+
+      expect(registrant).to receive(:renew_license)
+
+      @facility.renew_license(registrant)
     end
   end
 end
