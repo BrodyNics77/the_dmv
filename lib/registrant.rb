b0efdc593e978f9daff290fa3b0bf86ba9ead4f9
BrodyNics77 <brodynichols777@gmail.com>
2024-07-15 21:36:28 -0500

    tweaked register file

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,7 +1,9 @@
 class Registrant
-    attr_reader :registrant
+    attr_reader :name, :age, :active
 
-    def initialize 
-        @registrant :registrant
-        @
+    def initialize(params = {})
+        @name = params[:name]
+        @age = params[:age]
+        @active = params[:active]
     end
+end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -2,20 +2,25 @@ require 'spec_helper'
 
 RSpec.describe @registrant do
     before(:each) do
-      @registrant = Registrant.new({name: 'Brody, 25, true'})
-      @registrant = Registrant.new({name: 'Tj, 18, false'})
-      @registrant = Registrant.new({name: 'Theodora, 14, false'})
+      Registrant.new({name: 'Brody, 25, true'})
+      Registrant.new({name: 'Tj, 18, false'})
+      Registrant.new({name: 'Theodora, 14, false'})
 end
 
-describe '#initialze' do
-    it 'can initialze' do
-        expect(@registrant).to be_an_instance_of(Registrant)
-        expect(@registrant.) to eq([])
+describe '#initialize' do
+    it 'can initialize' do
+        @registrants.each do |registrant|
+        expect(registrant).to be_an_instance_of(Registrant)
+        end
     end
 end
 
-describe "#add registrants" do
+describe '#add registrants' do
     it 'can add new registrants' do
-        expect()
+        new_registrant = Registrant.new({name: 'Alice', age: 30, active: true})
+        @registrants << new_registrant
+  
+        expect(@registrants).to include(new_registrant)
+        end
     end
 end
