545ae695aa257b6a97f8f74df13562ae4312b37e
BrodyNics77 <brodynichols777@gmail.com>
2024-08-30 14:59:23 -0500

    Test: cleaned up previous test and added license data

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,20 +1,40 @@
 require './lib/registrant'
 
 Rspec.describe Registrant do
-    it 'is a registrant' do
+    it 'the registrant has a name' do
         registrant_1 = Registrant.new('Bruce', 18, true )
         registrant_2 = Registrant.new('Penny', 15 )
 
         expect(registrant_1.name).to eq("Bruce")
-        expect(registrant_1.age).to eq(18)
-        expect(registrant_1.permit).to eq(true)
+    #    expect(registrant_1.age).to eq(18)
+     #   expect(registrant_1.permit).to eq(true)
         expect(registrant_2.name).to eq("Penny")
+      #  expect(registrant_2.age).to eq(15)
+      #  expect(registrant_2.permit).to eq(false)
+    end
+
+    it 'has an age' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant_1.age).to eq(18)
         expect(registrant_2.age).to eq(15)
+    end
+
+    it 'has a permit?' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant_1.permit).to eq(true)
         expect(registrant_2.permit).to eq(false)
     end
 
-    it "has license data" do
+    it 'has license data' do
         registrant_1 = Registrant.new('Bruce', 18, true )
         registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant_1.license_data).to eq({})
+        expect(registrant_2.license_data).to eq({})
+    end
 
         
