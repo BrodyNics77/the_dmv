65843630e597f4ed9d571551e4336b80ebfcc3f5
Brody Nikolaos <brodynichols777@gmail.com>
2024-09-03 02:27:46 -0500

    Feat: added hash for license data

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,10 +1,11 @@
 class Registrant
-    attr_reader :name, :age
+    attr_reader :name, :age, :permit, :license_data
 
     def initialize(name, age)
         @name = name
         @age = age
         @permit = false
+        @license_data = {written: false, license: false, renewed: false}
     end
 
     def earn_permit
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -33,8 +33,9 @@ Rspec.describe Registrant do
         registrant_1 = Registrant.new('Bruce', 18, true )
         registrant_2 = Registrant.new('Penny', 15 )
 
-        expect(registrant_1.license_data).to eq({})
-        expect(registrant_2.license_data).to eq({})
+        expect(registrant_1.license_data).to eq({ written: false, license: false, renewed: false})
+        expect(registrant_2.license_data).to eq({ written: false, license: false, renewed: false})
     end
+end
 
         
