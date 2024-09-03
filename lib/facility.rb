ca5ab7022ebedde28d608340e02f36160569336b
BrodyNics77 <brodynichols777@gmail.com>
2024-09-03 18:00:33 -0500

    Feat: added most of first half of interation 2

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,15 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(attrs)
+    @name = attrs[:name]
+    @address = attrs[:address]
+    @phone = attrs[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
