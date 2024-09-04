eb9c9ece69064e9cd9618f8328bb140b6cf7b94d
Brody Nikolaos <brodynichols777@gmail.com>
2024-09-04 18:44:55 -0500

    Test: Added Vehicle_factory

---
+++ lib/vehicle_factory_spec.rb
@@ -0,0 +1,8 @@
+require './lib/vehicle'
+require './lib/vehicle_factory'
+require './lib/dmv_data_service'
+
+Rspec.describe Vehicle_factory do
+    before(:each) do
+        @factory = VehicleFactory.new
+        @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
